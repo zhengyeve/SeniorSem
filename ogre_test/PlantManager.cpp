3228e612a67d0fcf3c57bc71ffc1c75a88d71df1
Matthew Isom <isoma-13@rhodes.edu>
2012-11-19 19:27:46 -0600

    Added comments to the plant manager

--- ogre_test/PlantManager.cpp
+++ ogre_test/PlantManager.cpp
@@ -3,7 +3,8 @@
 
 PlantManager::PlantManager(void) {
 	//TODO: Use an XML file or something to read in these plant values
-	//Average flatness is about 0.2 on the test map
+
+	//See the PlantManager.h for a description of each variable.
 	PlantProfile oak;
 	oak.plantType = PLANT_OAK;
 	oak.frequency = 100;
@@ -39,44 +40,45 @@ PlantManager::PlantManager(void) {
 }
 
 PlantType PlantManager::getFlora(double height, float terrain_flatness) {
-	//cull about half... 
-	/*if (rand()%2) {
-		return PLANT_NONE;
-	}*/
-
-	/*static float average_flatness = 0;
+	//An average height calculation for debugging.
+	/*static double average_height = 0;
 	static double num_evaluated = 0;
-
-	if (terrain_flatness < 0.9 && terrain_flatness > 0.01) {
-		average_flatness = ((num_evaluated * average_flatness) + terrain_flatness) / (num_evaluated + 1.0);
+	if (height > 0) {
+		average_height = ((num_evaluated * average_height) + height) / (num_evaluated + 1.0);
 		num_evaluated++;
-		std::cout << "Average flatness: " << average_flatness << "\n";
+		std::cout << "Average height: " << average_height << "\n";
 		std::cout << "Number evaluated: " << num_evaluated << "\n";
 	}*/
 
 	PlantType winner = PLANT_NONE;
-	float highest_score = 20; //increase number to decrease the chance plants will spawn
-	float cur_score = 0;
+	float highest_score = 20; //increase number to decrease the chance plants will spawn / decrease the overall number of plants
 
 	for (unsigned int i = 0; i < plants.size(); ++i) {
-		cur_score = (plants[i].rangeHeight - abs(plants[i].idealHeight-height)) / plants[i].rangeHeight;
+		//subtracts the difference between the height and the ideal height from the range, and divides it by the range.
+		//thus, if the distance from the ideal is zero, the calculation will evaluate to a 1. If the difference is
+		//as large as the range, the calculation will evaluate to a 0. So, it will be a number from 0-1 that represents
+		//how close to ideal it is.
+		float cur_score = (plants[i].rangeHeight - abs(plants[i].idealHeight-height)) / plants[i].rangeHeight;
+		//multiply it by the weight to scale it by it's importance
 		cur_score *= plants[i].weightHeight;
-		//if it's negative (outside possible range), skip the next calculation step. In case multiple negative numbers combine to make a positive one...
+		//if it's negative (outside possible range), skip the next calculation step. So multiple negative numbers won't combine to make a positive one...
 		if (cur_score < 0) {
 			cur_score = 0;
 		} else {
+			//just like the height calculation.
 			cur_score *= ((plants[i].rangeFlatness - abs(plants[i].idealFlatness-terrain_flatness)) / plants[i].rangeFlatness) * plants[i].weightFlatness;
+			//divide by the weights, so the resulting number will be between 0 and 1.
 			cur_score /= plants[i].weightHeight * plants[i].weightFlatness;
 		}
-		/*if (plants[i].plantType == PLANT_ROUND_SHROOM && cur_score > 0) {
-			std::cout << "cur_score: " << cur_score << "\tcur flatness: " << terrain_flatness << "\n";
-		}*/
-		//make sure this hasn't become a negative number
+		//make sure this hasn't become a negative number by being outside the possible range
 		if (cur_score < 0) {
 			cur_score = 0;
 		}
-		//at this point, cur_score should be in the range 0.0-1.0
+		//multiply the score by a random value between zero and the plant's frequency. Here's where a higher frequency can help
+		//result in a higher score. The randomness is so that plant zones will interpenetrate somewhat, and not have a strict
+		//line in between them.
 		cur_score *= rand()%plants[i].frequency;
+		//if this is better than what we've gotten in previous runs, make it the new high score and winner
 		if (cur_score > highest_score) {
 			highest_score = cur_score;
 			winner = plants[i].plantType;
--- ogre_test/PlantManager.h
+++ ogre_test/PlantManager.h
@@ -7,15 +7,18 @@ enum PlantType{PLANT_NONE, PLANT_OAK, PLANT_PINE, PLANT_ROUND_SHROOM};
 
 struct PlantProfile {
 	PlantType plantType;
-	//the ideal value for each category
+	//the ideal value for each category. The average flatness for the test map is 0.2 and average height is 57.
 	double idealHeight;
 	float idealFlatness;
-	//the range for each category
+	//the maximum range for each category. If a plant has an idealHeight=50 and a rangeHeight=10, they won't be generated below 40 or above 60,
+	//and will be much more common at 50 that at 40.
 	double rangeHeight;
 	float rangeFlatness;
-	//the weights will be how important each category is
+	//the weights will be how important each category is, relative to one another. The exact numbers don't matter, rather the ratio is what
+	//determines the relative weight. weightHeight=1 and weightFlatness=2 is equivalent to weightHeight=10 and weightFlatness=20. The higher
+	//the number the more important the category is.
 	float weightHeight, weightFlatness;
-	//frequency of plant occurence (rarity)
+	//frequency of plant occurence (rarity). The higher the number, the more frequent it will be.
 	int frequency;
 };
 
@@ -25,7 +28,10 @@ private:
 	std::vector<PlantProfile> plants;
 
 public:
+	//initializes the plant profiles
 	PlantManager(void);
+	//Pass this the terrain height and flatness at a location and it will return to you a plant that occurs there. Sometimes returns PLANT_NONE
+	//if no plant ought to be placed at all.
 	PlantType getFlora(double height, float terrain_flatness);
 };
 
